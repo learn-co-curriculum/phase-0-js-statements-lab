a80d3299f37f42fb6981c066eddf3006f5d7af63
Camrin <camrindavis58@gmail.com>
2025-10-10 10:20:10 -0500

    Added watering,light,and soil control.

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,33 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//Watering Control:**
+ //  - Use an `if-else` statement to check the `temperature`.
+  // - If the `temperature` is greater than 80, console log `"Watering on"`.
+  // - Otherwise, console log `"Watering off"`.
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+
+  //**Garden Lights Control:**
+   //- Use an `if-else` statement to check `timeOfDay`.
+   //- If `timeOfDay` is `"evening"` or `"night"`, console log `"Lights on"`.
+  // - Otherwise, console log `"Lights off"`.
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+  //**Soil Moisture Adjustment:**
+  // - Use a `while` loop to increment the value of `soilMoisture` by 5 until it reaches 40.
+   //- Console log the value of `soilMoisture` at each step, and when it reaches 40, console log `40`.
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(soilMoisture);
+}
